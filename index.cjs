8460de3af38318a86bbb5e1995e50facd3b02606
Kristján Oddsson <koddsson@gmail.com>
2023-07-08 09:37:54 +0200

    Add recommended unicorn rules

--- index.cjs
+++ index.cjs
@@ -1,15 +1,20 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2022: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": ["./tsconfig.json"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:unicorn/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true
-}
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "unicorn"],
+  root: true,
+};
