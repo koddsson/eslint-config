0f454dd363a84203f0e258278c360d018398c58a
Kristján Oddsson <koddsson@gmail.com>
2024-04-26 11:02:34 +0200

    set up tscompat

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import js from "@eslint/js";
 import globals from "globals";
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import tscompat from "@koddsson/eslint-plugin-tscompat";
 
 import parser from "@typescript-eslint/parser";
 
-// mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -26,6 +26,28 @@ export default await (async function () {
     eslintPluginUnicorn.configs["flat/all"],
     ...compat.extends("plugin:@typescript-eslint/recommended"),
     eslintPluginPrettierRecommended,
+    {
+      plugins: {
+        tscompat,
+      },
+      rules: {
+        "tscompat/tscompat": [
+          "error",
+          {
+            browserslist: [
+              "last 10 chrome versions",
+              "last 10 ChromeAndroid versions",
+              "last 10 edge versions",
+              "last 10 samsung versions",
+              "last 10 firefox versions",
+              "last 2 safari major versions",
+              "last 2 ios major versions",
+              "not dead",
+            ],
+          },
+        ],
+      },
+    },
     {
       languageOptions: {
         ecmaVersion: 2022,
